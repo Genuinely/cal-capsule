1c84116c17e8eacd78ed47ed45cbbbab68b986b0
Neil Yeung <nyeung@dhcp-vl2051-806.eduroam.cornell.edu>
2023-12-03 02:10:19 -0500

    first animation done

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-import CountUp from 'countup.js';
+document.addEventListener('DOMContentLoaded', function () {
+    var frame1 = document.getElementById('frame1');
+    var frame2 = document.getElementById('frame2');
+    var frame3 = document.getElementById('frame3');
 
-// Initialize CountUp instance
-const countUp = new CountUp('counting-number', 0, 1000, 0, 2);
+    window.addEventListener('scroll', function() {
+        var scroll = window.scrollY;
 
-// Intersection Observer setup
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting && !countUp.error) {
-      // Start counting animation when the element is in view
-      countUp.start();
-      observer.unobserve(entry.target); // Stop observing once the element is shown
-    }
-  });
+        // Adjust these values based on your page's scroll height
+        var frameHeight = window.innerHeight;
+        var firstFrameEnd = frameHeight;
+        var secondFrameEnd = frameHeight * 2;
+
+        if (scroll < firstFrameEnd) {
+            anime({
+                targets: frame1,
+                opacity: 1,
+                easing: 'linear',
+                duration: 500
+            });
+            frame2.style.opacity = 0;
+            frame3.style.opacity = 0;
+        } else if (scroll < secondFrameEnd) {
+            frame1.style.opacity = 0;
+            anime({
+                targets: frame2,
+                opacity: 1,
+                easing: 'linear',
+                duration: 500
+            });
+            frame24.style.opacity = 0;
+        } else {
+            frame1.style.opacity = 0;
+            frame2.style.opacity = 0;
+            anime({
+                targets: frame3,
+                opacity: 1,
+                easing: 'linear',
+                duration: 500
+            });
+        }
+    });
 });
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
