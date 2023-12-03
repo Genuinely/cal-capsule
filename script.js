5a88e7a793edcef1aac55eb5c34e4397ec2874b8
Neil Yeung <nyeung@dhcp-vl2051-806.eduroam.cornell.edu>
2023-12-03 01:53:49 -0500

    reverted to a simpler setup; stuff not working still

--- script.js
+++ script.js
@@ -1,64 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const element = document.querySelector('.text-animation');
-    const lettersHtml = element.textContent.replace(/\S/g, '<span class="letter">$&</span>');
-    element.innerHTML = `<div class="letters">${lettersHtml}</div><span class="cursor"></span>`;
-    element.style.display = 'block';
+import CountUp from 'countup.js';
 
-    // Typewriter animation code
-    const letters = Array.from(element.querySelectorAll('.letter'));
-    const TYPE_AFTER_MS = 2000; // Slower typing speed
-    const JUMP_AFTER_MS = 100; // Slower delay between letters
+// Initialize CountUp instance
+const countUp = new CountUp('counting-number', 0, 1000, 0, 2);
 
-    const blink = anime({
-        targets: '.text-animation .cursor',
-        duration: 750,
-        opacity: [
-            { value: 1, delay: 0 },
-            { value: 0, delay: 375 }
-        ],
-    });
-
-    const animation = anime.timeline({
-        autoplay: false // Pause the animation by default
-    })
-        .add({
-            targets: '.text-animation .cursor',
-            translateX: letters.map((letter, i) =>
-                ({ value: letter.offsetLeft + letter.offsetWidth, duration: 1, delay: i === 0 ? 0 : JUMP_AFTER_MS })),
-        }, TYPE_AFTER_MS)
-        .add({
-            targets: '.text-animation .letter',
-            opacity: [0, 1],
-            duration: 100, // Slightly slower
-            delay: anime.stagger(JUMP_AFTER_MS),
-            changeBegin: () => {
-                blink.reset();
-                blink.pause();
-            },
-            changeComplete: () => {
-                blink.restart();
-            },
-        }, TYPE_AFTER_MS);
-
-    // Add an event listener for scrolling
-    window.addEventListener("scroll", function () {
-        const scrollThreshold = 500; // Adjust this value to determine when to show the scroll content
-        const scrollContent = document.querySelector('.scroll-content');
-
-        // Calculate the scroll progress as a percentage
-        const scrollPercent = (window.scrollY / scrollThreshold) * 100;
-
-        // Seek the animation based on scroll progress
-        animation.seek((scrollPercent / 100) * animation.duration);
-
-        // Check if the user has scrolled past the threshold to show the scroll content
-        if (window.scrollY > scrollThreshold) {
-            scrollContent.style.display = 'block';
-        } else {
-            scrollContent.style.display = 'none';
-        }
-    });
-
-    // Start the animation when the page loads
-    animation.play();
+// Intersection Observer setup
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && !countUp.error) {
+      // Start counting animation when the element is in view
+      countUp.start();
+      observer.unobserve(entry.target); // Stop observing once the element is shown
+    }
+  });
 });
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
