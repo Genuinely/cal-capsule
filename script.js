0276895312ac68bc630a194f67009e2528c4e72a
Neil Yeung <nyeung@dhcp-vl2051-806.eduroam.cornell.edu>
2023-12-03 04:03:53 -0500

    got the number animation working

--- script.js
+++ script.js
@@ -2,8 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
     var frame1 = document.getElementById('frame1');
     var frame2 = document.getElementById('frame2');
     var frame3 = document.getElementById('frame3');
+    var frame4 = document.getElementById('frame4');
+    var countingNumbers = document.getElementById('counting-numbers');
 
-    window.addEventListener('scroll', function() {
+    // Set the scroll position to trigger the count-up animation
+    var triggerScrollPosition = 2853;
+
+    function startCounting() {
+        const counters = document.querySelectorAll('.counter');
+
+        counters.forEach((counter) => {
+            anime({
+                targets: counter,
+                innerHTML: [0, counter.getAttribute('data-count')],
+                easing: 'easeInOutSine',
+                round: 1,
+                duration: 2000,
+            });
+        });
+    }
+
+    window.addEventListener('scroll', function () {
         var scroll = window.scrollY;
 
         // Adjust these values based on your page's scroll height
@@ -11,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var firstFrameEnd = frameHeight;
         var secondFrameEnd = frameHeight * 2;
 
-        if (scroll < firstFrameEnd) {
+        if (scroll < firstFrameEnd - 20) {
             anime({
                 targets: frame1,
                 opacity: 1,
@@ -20,6 +39,9 @@ document.addEventListener('DOMContentLoaded', function () {
             });
             frame2.style.opacity = 0;
             frame3.style.opacity = 0;
+
+            // Ensure any counting stops when leaving the first frame
+            // startCounting(); // Start counting animation if needed
         } else if (scroll < secondFrameEnd) {
             frame1.style.opacity = 0;
             anime({
@@ -28,16 +50,34 @@ document.addEventListener('DOMContentLoaded', function () {
                 easing: 'linear',
                 duration: 500
             });
-            frame24.style.opacity = 0;
-        } else {
+            frame3.style.opacity = 0;
+
+            // Ensure any counting stops when leaving the second frame
+            // startCounting(); // Start counting animation if needed
+        } else if (scroll >= triggerScrollPosition) {
             frame1.style.opacity = 0;
             frame2.style.opacity = 0;
             anime({
                 targets: frame3,
                 opacity: 1,
                 easing: 'linear',
+                duration: 500,
+                complete: function() {
+                    // Start counting animation when scrolling past the trigger point for frame 3
+                    startCounting();
+                }
+            });
+        } else {
+            // Transition back to frame4 when scrolling past the trigger point
+            anime({
+                targets: frame4,
+                opacity: 1,
+                easing: 'linear',
                 duration: 500
             });
+
+            // Ensure any counting stops when leaving frame4
+            startCounting(); // Start counting animation if needed
         }
     });
 });
